30dd17a25ae829cf8891f093ee7d1c2e01dad6f4
Kieran Dunch <dunch.kieran@gmail.com>
2022-08-19 16:45:21 -0400

    finished #12

--- spec/12_magic_seven_spec.rb
+++ spec/12_magic_seven_spec.rb
@@ -70,9 +70,23 @@ describe MagicSeven do
   # Write a test for each of the following methods:
 
   describe '#subtract_four' do
+    context " when the previous step is 16" do
+      it "returns 12" do
+        previous_step = 16
+        result = game.subtract_four(previous_step)
+        expect(result).to eq(12)
+      end
+    end
   end
 
   describe '#divide_by_two' do
+    context " when the previous step is 12" do
+      it "returns 12" do
+        previous_step = 12
+        result = game.divide_by_two(previous_step)
+        expect(result).to eq(6)
+      end
+    end
   end
 
   # The following tests will need you to create new instances of MagicSeven with
@@ -85,19 +99,22 @@ describe MagicSeven do
   describe '#play' do
     context 'when the random number is ...' do
       # remove the 'x' before running this test
-      xit 'will return 7' do
+      it 'will return 7' do
+        expect(game.play).to eq(7)
       end
     end
 
     context 'when the random number is ...' do
       # remove the 'x' before running this test
-      xit 'will return 7' do
+      it 'will return 7' do
+        expect(game.play).to eq(7)
       end
     end
 
     context 'when the random number is ...' do
       # remove the 'x' before running this test
-      xit 'will return 7' do
+      it 'will return 7' do
+        expect(game.play).to eq(7)
       end
     end
   end
